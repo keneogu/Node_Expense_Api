cf1fbf2d6005ccb682de647789f77144fdbe2e43
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-07-25 22:02:18 +0100

    Add token handler

---
+++ middleware/tokenHandler.js
@@ -0,0 +1,14 @@
+const tokenHandler = (user, statusCode, res) => {
+  const token = user.getJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + 7 * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  res.status(statusCode).cookie('token', token, options).json({ user, token });
+};
+
+module.exports = tokenHandler;
