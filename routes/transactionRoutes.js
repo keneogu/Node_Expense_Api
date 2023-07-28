9ed2cf24b299d557f57d2cbb387c7962f2f1c487
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-07-28 10:56:48 +0100

    Add delete Transaction route

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const { getTransactions, createTransaction } = require('../controllers/transactionController');
+const express = require("express");
+const {
+  getTransactions,
+  createTransaction,
+  deleteTransaction,
+} = require("../controllers/transactionController");
 const router = express.Router();
 
-const {authenticateUser} = require('../middleware/authenticate');
+const { authenticateUser } = require("../middleware/authenticate");
 
-router.route('/transaction').get(getTransactions);
-router.route('/transac').post(authenticateUser, createTransaction);
+router.use(authenticateUser);
+router.route("/transaction").get(getTransactions);
+router.route("/transac").post(createTransaction);
+router.route("/transaction/:id").delete(deleteTransaction);
 
 module.exports = router;
