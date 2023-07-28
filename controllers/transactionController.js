c62426e0e263c3645575ffa7c2914a9cef30830e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-07-28 10:56:20 +0100

    Add deleteTransaction action to delete a specific transaction

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,41 +1,56 @@
 const Transaction = require("../models/transactionModel");
 const asyncHandler = require("express-async-handler");
 
-const getTransactions = asyncHandler(async (req,res) => {
-	const transactions = await Transaction.find();
-	console.log(req.body);
-	const count = await Transaction.countDocuments();
-	res.status(200).json({
-		success: true,
-		count,
-		data: transactions
-	})
-})
+const getTransactions = asyncHandler(async (req, res) => {
+  const transactions = await Transaction.find({ user_id: req.user.id });
+  const count = await Transaction.countDocuments();
+  res.status(200).json({
+    success: true,
+    count,
+    data: transactions,
+  });
+});
 
-const createTransaction = asyncHandler(async(req,res) => {
+const createTransaction = asyncHandler(async (req, res) => {
+  const { amount, category, type, desc } = req.body;
 
-	const {amount, category, type, desc} = req.body
+  if (!amount || !category || !type || !desc) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
 
-	// const transaction = await Transaction.create(req.body)
-	if(!amount || !category || !type || !desc) {
-		res.status(400)
-		throw new Error("All fields are mandatory")
-	}
+  const transaction = await Transaction.create({
+    amount,
+    category,
+    type,
+    desc,
+    user_id: req.user.id,
+  });
 
-	const transaction = await Transaction.create({
-		amount,
-		category,
-		type,
-		desc,
-		user_id: req.user.id
-	})
+  console.log(req.user);
+  res.status(201).json({
+    success: true,
+    data: transaction,
+  });
+});
 
-	console.log(req.user)
-	res.status(201).json({
-		success: true,
-		data: transaction
+const deleteTransaction = asyncHandler(async (req, res) => {
+  const transaction = await Transaction.findById(req.params.id);
+	
+  if (!transaction) {
+    res.status(404);
+    throw new Error("Transaction not found");
+  }
+
+  if (transaction.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission for this action");
+  }
+
+  await Transaction.deleteOne({ _id: req.params.id });
+  res.status(200).json({
+		success: true
 	});
+});
 
-})
-
-module.exports = {getTransactions, createTransaction}
+module.exports = { getTransactions, createTransaction, deleteTransaction };
