883b3e1464fe26f639cb99786726daedf7672096
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-08-11 23:16:06 +0100

    Add logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/auth');
+const { registerUser, loginUser, logoutUser } = require('../controllers/auth');
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/logout').get(logoutUser);
 
 module.exports = router;
